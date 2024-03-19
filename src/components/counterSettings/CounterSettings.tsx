1cf59348e68c1aafb60c0e8f990add2182a606b1
DinaLysenko <dinafate@rambler.ru>
2024-03-19 22:55:54 +0500

    add CounterSettings.tsx

---
+++ src/components/counterSettings/CounterSettings.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import s from './CounterSettings.module.css'
+import {ValueInput} from '../valueInput/ValueInput';
+
+
+export const CounterSettings = () => {
+    return (
+        <div className={s.settings}>
+            <ValueInput labelTitle={'max value:'}/>
+            <ValueInput labelTitle={'min value:'}/>
+
+
+        </div>
+    );
+};
+
