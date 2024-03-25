23385507b30504edec092ea1c5ca29512a94062f
DinaLysenko <dinafate@rambler.ru>
2024-03-25 23:46:16 +0500

    add CounterSettings.tsx

--- src/components/counterSettings/CounterSettings.tsx
+++ src/components/counterSettings/CounterSettings.tsx
@@ -1,14 +1,41 @@
 import React from 'react';
 import s from './CounterSettings.module.css'
-import {ValueInput} from '../valueInput/ValueInput';
+import {InputValue} from '../inputValue/InputValue';
+import {Button} from '../button/Button';
 
 
-export const CounterSettings = () => {
+type CounterSettingsType = {
+    startValue: number
+    maxValue: number
+    setMaxValue: (maxValue: number) => void
+    setStartValue: (startValue: number) => void
+    disabledButton: boolean
+    setDisabledButton: (disabledButton:boolean)=>void
+    onClickSetButton: ()=>void
+}
+export const CounterSettings = ({
+                                    startValue,
+                                    maxValue,
+                                    setStartValue,
+                                    setMaxValue,
+                                    disabledButton, setDisabledButton, onClickSetButton
+                                }: CounterSettingsType) => {
+
+
+
     return (
-        <div className={s.settings}>
-            <ValueInput labelTitle={'max value:'}/>
-            <ValueInput labelTitle={'min value:'}/>
+        <div className={'wrapper'}>
+            <div className={s.settings}>
+                <span className={s.spanTitle}>max value</span>
+                <InputValue value={maxValue} onChange={setMaxValue}/>
+                <span className={s.spanTitle}>start value</span>
+                <InputValue value={startValue} onChange={setStartValue}/>
+            </div>
+            <div className={'buttonWrapper'}>
+                <Button name={'Set'} onClick={onClickSetButton} disabled={maxValue === 5 && startValue === 0? disabledButton: !disabledButton}/>
+            </div>
         </div>
-    );
+    )
+        ;
 };
 
